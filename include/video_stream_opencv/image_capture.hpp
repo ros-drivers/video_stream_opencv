cb489ecf818ff1f53f317708bea722edc2a12907
Kunal Tyagi <tyagi.kunal@live.com>
2018-06-25 12:38:44 +0900

    Separated out image capturing code for modularity

---
+++ include/video_stream_opencv/image_capture.hpp
@@ -0,0 +1,148 @@
+#ifndef _VIDEO_STREAM_CAPTURE_IMAGE_CAPTURE_HPP_
+#define _VIDEO_STREAM_CAPTURE_IMAGE_CAPTURE_HPP_
+
+#include <algorithm>
+#include <fstream>
+#include <string>
+
+#include <boost/thread/sync_queue.hpp>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/videoio/videoio.hpp>
+
+namespace video_stream {
+namespace {
+const double _CAPTURE_FPS = 30;
+const unsigned int _MAX_QUEUE_SIZE = 100;
+const bool _DEFAULT_LOOP = false;
+}
+
+struct ImageCapture
+{
+    const double camera_fps = _CAPTURE_FPS;
+
+    ImageCapture(const std::string &stream_path, double fps,
+                 unsigned int queue_size, bool loop_file = _DEFAULT_LOOP):
+        camera_fps(fps),
+        max_queue_size_(queue_size),
+        loop_(loop_file),
+        stream_path_(stream_path) {
+        video_stream_provider_ = StreamType::UNKNOWN;
+        // same size limit used internally in cv::VideoCapture
+        if (stream_path.size() < 4) {
+            video_stream_provider_ = StreamType::DEV_VIDEO;
+        }
+        else if (stream_path.rfind("http://", 0) == 0 ||
+                 stream_path.rfind("https://", 0) == 0) {
+            video_stream_provider_ = StreamType::HTTP;
+        }
+        else if (stream_path.rfind("rtsp://", 0) == 0) {
+            video_stream_provider_ = StreamType::RTSP;
+        }
+        // one single % is used to identify img_sequence patterns by cv
+        else if (std::count(stream_path.begin(), stream_path.end(), '%') == 1) {
+            video_stream_provider_ = StreamType::IMG_SEQ;
+        }
+        else {
+            std::ifstream ifs(stream_path);
+            // Check if file exists to know if it's a valid file
+            if (ifs.good()) {
+                // can be a video or image file, both are valid
+                video_stream_provider_ = StreamType::MEDIA_FILE;
+            }
+        }
+        open_();
+    }
+
+    ImageCapture(const std::string &stream_path, double fps = _CAPTURE_FPS):
+        ImageCapture(stream_path, fps, _MAX_QUEUE_SIZE, _DEFAULT_LOOP)
+    {}
+
+    ImageCapture(const std::string &stream_path, unsigned int queue_size):
+        ImageCapture(stream_path, _CAPTURE_FPS, queue_size, _DEFAULT_LOOP)
+    {}
+
+    ImageCapture(const std::string &stream_path, bool loop):
+        ImageCapture(stream_path, _CAPTURE_FPS, _MAX_QUEUE_SIZE, loop)
+    {}
+
+    ImageCapture(const std::string &stream_path, double fps, bool loop):
+        ImageCapture(stream_path, fps, _MAX_QUEUE_SIZE, loop)
+    {}
+
+    bool isFile() {
+        return (video_stream_provider_ == StreamType::MEDIA_FILE ||
+                video_stream_provider_ == StreamType::IMG_SEQ);
+    }
+
+    // wrap get, set, grab, retrieve, read and >> for VideoCapture
+    bool grab() {
+        bool ret = cap_.grab();
+        if (ret == false && loop_ == true)
+        {
+            // nothing has changed wrt StreamType
+            open_();
+            return grab();
+        }
+        return ret;
+    }
+
+    bool retrieve(cv::OutputArray &arr) {
+        bool ret = cap_.retrieve(arr);
+        return ret;
+    }
+
+    bool read(cv::OutputArray &arr) {
+        if (grab()) {
+            return retrieve(arr);
+        }
+        return false;
+    }
+
+    ImageCapture& operator>> (cv::UMat &img) {
+        read(img);
+        return *this;
+    }
+
+    ImageCapture& operator>> (cv::Mat &img) {
+        read(img);
+        return *this;
+    }
+
+    // wrap pull, push for framesQueue_
+    bool push(const cv::Mat &img) {
+        bool full = false;
+        if (framesQueue_.size() >= max_queue_size_) {
+            full = true;
+            cv::Mat temp;
+            framesQueue_.pull(temp);
+        }
+        framesQueue_.push(img);
+        return full;
+    }
+
+    void pull(cv::Mat &img) {
+        framesQueue_.pull(img);
+    }
+
+    private:
+        mutable boost::sync_queue<cv::Mat> framesQueue_;
+        mutable cv::VideoCapture cap_;
+
+        const unsigned int max_queue_size_ = _MAX_QUEUE_SIZE;
+        const bool loop_ = _DEFAULT_LOOP;
+        const std::string stream_path_;
+
+        enum class StreamType { UNKNOWN, DEV_VIDEO, HTTP, RTSP, MEDIA_FILE, IMG_SEQ };
+        StreamType video_stream_provider_;
+
+        bool open_() {
+            if (video_stream_provider_ == StreamType::DEV_VIDEO) {
+                return cap_.open(atoi(stream_path_.c_str()));
+            }
+            return cap_.open(stream_path_);
+        }
+};
+}  // namespace video_stream
+
+#endif  // ifndef _VIDEO_STREAM_CAPTURE_IMAGE_CAPTURE_HPP_
--- src/video_stream.cpp
+++ src/video_stream.cpp
@@ -34,80 +34,62 @@
  * @author Sammy Pfeiffer
  */
 
+#include <sstream>
+#include <fstream>
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <camera_info_manager/camera_info_manager.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <sstream>
-#include <fstream>
+
 #include <boost/assign/list_of.hpp>
 #include <boost/thread/thread.hpp>
-#include <boost/thread/sync_queue.hpp>
 
-boost::sync_queue<cv::Mat> framesQueue;
-cv::VideoCapture cap;
-std::string video_stream_provider_type;
-double set_camera_fps;
-int max_queue_size;
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 // Based on the ros tutorial on transforming opencv images to Image messages
-
-sensor_msgs::CameraInfo get_default_camera_info_from_image(sensor_msgs::ImagePtr img) {
+sensor_msgs::CameraInfo get_default_camera_info_from_image_msg(sensor_msgs::ImagePtr img) {
     sensor_msgs::CameraInfo cam_info_msg;
-    cam_info_msg.header.frame_id = img->header.frame_id;
+    cam_info_msg.header = img->header;
     // Fill image size
     cam_info_msg.height = img->height;
     cam_info_msg.width = img->width;
-    ROS_INFO_STREAM("The image width is: " << img->width);
-    ROS_INFO_STREAM("The image height is: " << img->height);
+
     // Add the most common distortion model as sensor_msgs/CameraInfo says
     cam_info_msg.distortion_model = "plumb_bob";
     // Don't let distorsion matrix be empty
     cam_info_msg.D.resize(5, 0.0);
     // Give a reasonable default intrinsic camera matrix
-    cam_info_msg.K = boost::assign::list_of(1.0) (0.0) (img->width/2.0)
-                                           (0.0) (1.0) (img->height/2.0)
-                                           (0.0) (0.0) (1.0);
+    cam_info_msg.K = {1.0, 0.0, img->width/2.0,
+                      0.0, 0.0, img->height/2.0,
+                      0.0, 0.0, 1.0};
     // Give a reasonable default rectification matrix
-    cam_info_msg.R = boost::assign::list_of (1.0) (0.0) (0.0)
-                                            (0.0) (1.0) (0.0)
-                                            (0.0) (0.0) (1.0);
+    cam_info_msg.R = {1.0, 0.0, 0.0,
+                      0.0, 1.0, 0.0,
+                      0.0, 0.0, 1.0};
     // Give a reasonable default projection matrix
-    cam_info_msg.P = boost::assign::list_of (1.0) (0.0) (img->width/2.0) (0.0)
-                                            (0.0) (1.0) (img->height/2.0) (0.0)
-                                            (0.0) (0.0) (1.0) (0.0);
+    cam_info_msg.P = {1.0, 0.0, img->width/2.0, 0.0
+                      0.0, 0.0, img->height/2.0, 0.0
+                      0.0, 0.0, 1.0, 0.0};
     return cam_info_msg;
 }
 
-
-void do_capture(ros::NodeHandle &nh) {
+void do_capture(const ImageCapture& cap) {
+    ros::Rate camera_fps_rate(cap.camera_fps);
     cv::Mat frame;
-    cv::Mat _drop_frame;
-
-    ros::Rate camera_fps_rate(set_camera_fps);
 
     // Read frames as fast as possible
-    while (nh.ok()) {
+    while (ros::ok()) {
         cap >> frame;
-        if (video_stream_provider_type == "videofile") {
+        if (cap.isFile()) {
             camera_fps_rate.sleep();
         }
         if(!frame.empty()) {
-            // accumulate only until max_queue_size
-            if (framesQueue.size() < max_queue_size) {
-                framesQueue.push(frame.clone());
-            }
-            // once reached, drop the oldest frame
-            else {
-                framesQueue.pull(_drop_frame);
-                framesQueue.push(frame.clone());
-            }
+            cap.push(frame.clone());
         }
     }
 }
-
 
 int main(int argc, char** argv)
 {
@@ -121,50 +103,19 @@ int main(int argc, char** argv)
     std::string video_stream_provider;
     if (_nh.getParam("video_stream_provider", video_stream_provider)) {
         ROS_INFO_STREAM("Resource video_stream_provider: " << video_stream_provider);
-        // If we are given a string of 4 chars or less (I don't think we'll have more than 100 video devices connected)
-        // treat is as a number and act accordingly so we open up the videoNUMBER device
-        if (video_stream_provider.size() < 4) {
-            ROS_INFO_STREAM("Getting video from provider: /dev/video" << video_stream_provider);
-            video_stream_provider_type = "videodevice";
-            cap.open(atoi(video_stream_provider.c_str()));
-        }
-        else {
-            ROS_INFO_STREAM("Getting video from provider: " << video_stream_provider);
-            if (video_stream_provider.find("http://") != std::string::npos ||
-                video_stream_provider.find("https://") != std::string::npos) {
-                video_stream_provider_type = "http_stream";
-            }
-            else if(video_stream_provider.find("rtsp://") != std::string::npos) {
-                video_stream_provider_type = "rtsp_stream";
-            }
-            else {
-                // Check if file exists to know if it's a videofile
-                std::ifstream ifs;
-                ifs.open(video_stream_provider.c_str(), std::ifstream::in);
-                if (ifs.good()) {
-                    video_stream_provider_type = "videofile";
-                }
-                else {
-                    video_stream_provider_type = "unknown";
-                }
-            }
-            cap.open(video_stream_provider);
-        }
     }
     else {
         ROS_ERROR("Failed to get param 'video_stream_provider'");
         return -1;
     }
 
-    ROS_INFO_STREAM("Video stream provider type detected: " << video_stream_provider_type);
-
     std::string camera_name;
     _nh.param("camera_name", camera_name, std::string("camera"));
     ROS_INFO_STREAM("Camera name: " << camera_name);
 
-    _nh.param("set_camera_fps", set_camera_fps, 30.0);
-    ROS_INFO_STREAM("Setting camera FPS to: " << set_camera_fps);
-    cap.set(CV_CAP_PROP_FPS, set_camera_fps);
+    _nh.param("set_camera_fps", camera_fps, 30.0);
+    ROS_INFO_STREAM("Setting camera FPS to: " << camera_fps);
+    cap.set(CV_CAP_PROP_FPS, camera_fps);
 
     double reported_camera_fps;
     // OpenCV 2.4 returns -1 (instead of a 0 as the spec says) and prompts an error
@@ -186,9 +137,9 @@ int main(int argc, char** argv)
     _nh.param("fps", fps, 240.0);
     ROS_INFO_STREAM("Throttling to fps: " << fps);
 
-    if (video_stream_provider.size() < 4 && fps > set_camera_fps) {
+    if (video_stream_provider.size() < 4 && fps > camera_fps) {
         ROS_WARN_STREAM("Asked to publish at 'fps' (" << fps <<
-                        ") which is higher than the 'set_camera_fps' (" << set_camera_fps <<
+                        ") which is higher than the 'camera_fps' (" << camera_fps <<
                         "), we can't publish faster than the camera provides images.");
     }
 
@@ -273,7 +224,7 @@ int main(int argc, char** argv)
                 // Create a default camera info if we didn't get a stored one on initialization
                 if (cam_info_msg.distortion_model == "") {
                     ROS_WARN_STREAM("No calibration file given, publishing a reasonable default camera info.");
-                    cam_info_msg = get_default_camera_info_from_image(msg);
+                    cam_info_msg = get_default_camera_info_from_image_msg(msg);
                     cam_info_manager.setCameraInfo(cam_info_msg);
                 }
                 // The timestamps are in sync thanks to this publisher
