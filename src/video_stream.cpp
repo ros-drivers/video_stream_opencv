f1fc5d4445777980324a0fc1b45e1a8f5190f68b
Yuki Furuta <me@furushchev.ru>
2019-04-11 21:50:10 +0900

    support dynamic_reconfigure

--- src/video_stream.cpp
+++ src/video_stream.cpp
@@ -36,18 +36,22 @@
 
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
+#include <dynamic_reconfigure/server.h>
 #include <image_transport/image_transport.h>
 #include <camera_info_manager/camera_info_manager.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sstream>
-#include <fstream>
+#include <stdexcept>
+#include <boost/filesystem.hpp>
 #include <boost/assign/list_of.hpp>
 #include <boost/thread/thread.hpp>
 #include <queue>
 #include <mutex>
+#include <video_stream_opencv/VideoStreamConfig.h>
 
+namespace fs = boost::filesystem;
 
 namespace video_stream_opencv {
 
@@ -55,22 +59,25 @@ class VideoStreamNodelet: public nodelet::Nodelet {
 protected:
 boost::shared_ptr<ros::NodeHandle> nh, pnh;
 image_transport::CameraPublisher pub;
-std::mutex q_mutex;
+boost::shared_ptr<dynamic_reconfigure::Server<VideoStreamConfig> > dyn_srv;
+std::mutex q_mutex, s_mutex;
 std::queue<cv::Mat> framesQueue;
 cv::Mat frame;
 boost::shared_ptr<cv::VideoCapture> cap;
 std::string video_stream_provider;
 std::string video_stream_provider_type;
+std::string camera_name;
+std::string camera_info_url;
 std::string frame_id;
 double set_camera_fps;
 double fps;
 int max_queue_size;
 bool loop_videofile;
-bool is_subscribed;
+int subscriber_num;
 int width_target;
 int height_target;
-bool flip_image;
-int flip_value;
+bool flip_horizontal;
+bool flip_vertical;
 boost::thread capture_thread;
 ros::Timer publish_timer;
 sensor_msgs::CameraInfo cam_info_msg;
@@ -112,7 +119,12 @@ virtual void do_capture() {
 
     int frame_counter = 0;
     // Read frames as fast as possible
-    while (nh->ok() && is_subscribed) {
+    while (nh->ok() && subscriber_num > 0) {
+        if (!cap->isOpened()) {
+          NODELET_WARN("Waiting for device...");
+          cv::waitKey(100);
+          continue;
+        }
         if (!cap->read(frame))
         {
           NODELET_FATAL("Could not capture frame!");
@@ -170,9 +182,17 @@ virtual void do_publish(const ros::TimerEvent& event) {
 
     // Check if grabbed frame is actually filled with some content
     if(!frame.empty()) {
+        // From http://docs.opencv.org/modules/core/doc/operations_on_arrays.html#void flip(InputArray src, OutputArray dst, int flipCode)
+        // FLIP_HORIZONTAL == 1, FLIP_VERTICAL == 0 or FLIP_BOTH == -1
         // Flip the image if necessary
-        if (flip_image && is_new_image)
-            cv::flip(frame, frame, flip_value);
+        if (is_new_image){
+          if (flip_horizontal && flip_vertical)
+            cv::flip(frame, frame, -1);
+          else if (flip_horizontal)
+            cv::flip(frame, frame, 1);
+          else if (flip_vertical)
+            cv::flip(frame, frame, 0);
+        }
         msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
         // Create a default camera info if we didn't get a stored one on initialization
         if (cam_info_msg.distortion_model == ""){
@@ -188,13 +208,19 @@ virtual void do_publish(const ros::TimerEvent& event) {
 virtual void subscribe() {
   ROS_DEBUG("Subscribe");
   cap.reset(new cv::VideoCapture);
-  if (video_stream_provider_type == "videodevice") {
-    NODELET_INFO_STREAM("Opening VideoCapture with provider: /dev/video" << video_stream_provider);
-    cap->open(std::stoi(video_stream_provider));
-  } else {
+  try {
+    int device_num = std::stoi(video_stream_provider);
+    NODELET_INFO_STREAM("Opening VideoCapture with provider: /dev/video" << device_num);
+    cap->open(device_num);
+  } catch (std::invalid_argument &ex) {
     NODELET_INFO_STREAM("Opening VideoCapture with provider: " << video_stream_provider);
     cap->open(video_stream_provider);
+    if (!cap->isOpened()) {
+      NODELET_FATAL_STREAM("Invalid 'video_stream_provider': " << video_stream_provider);
+      return;
+    }
   }
+  NODELET_INFO_STREAM("Video stream provider type detected: " << video_stream_provider_type);
 
   double reported_camera_fps;
   // OpenCV 2.4 returns -1 (instead of a 0 as the spec says) and prompts an error
@@ -214,37 +240,43 @@ virtual void subscribe() {
     cap->set(CV_CAP_PROP_FRAME_WIDTH, width_target);
     cap->set(CV_CAP_PROP_FRAME_HEIGHT, height_target);
   }
-  is_subscribed = true;
+
   try {
     capture_thread = boost::thread(
       boost::bind(&VideoStreamNodelet::do_capture, this));
     publish_timer = nh->createTimer(
       ros::Duration(1.0 / fps), &VideoStreamNodelet::do_publish, this);
   } catch (std::exception& e) {
-    NODELET_ERROR_STREAM(e.what());
-    is_subscribed = false;
+    NODELET_ERROR_STREAM("Failed to start capture thread: " << e.what());
   }
 }
 
 virtual void unsubscribe() {
   ROS_DEBUG("Unsubscribe");
   publish_timer.stop();
-  is_subscribed = false;
   capture_thread.join();
   cap.reset();
 }
 
 virtual void connectionCallbackImpl() {
+  std::lock_guard<std::mutex> lock(s_mutex);
+  if (subscriber_num == 0) {
+    subscribe();
+  }
+  subscriber_num++;
+}
+
+virtual void disconnectionCallbackImpl() {
+  std::lock_guard<std::mutex> lock(s_mutex);
   bool always_subscribe = false;
   pnh->getParamCached("always_subscribe", always_subscribe);
-  if ((video_stream_provider == "videofile" || always_subscribe) && !is_subscribed) {
-    subscribe();
-  } else {
-    if (pub.getNumSubscribers() > 0) {
-    if (!is_subscribed) subscribe();
-    } else {
-      if (is_subscribed) unsubscribe();
-    }
+  if (video_stream_provider == "videofile" || always_subscribe) {
+    return;
+  }
+
+  subscriber_num--;
+  if (subscriber_num == 0) {
+    unsubscribe();
   }
 }
 
@@ -256,132 +288,140 @@ virtual void infoConnectionCallback(const ros::SingleSubscriberPublisher&) {
   connectionCallbackImpl();
 }
 
+virtual void disconnectionCallback(const image_transport::SingleSubscriberPublisher&) {
+  disconnectionCallbackImpl();
+}
 
-virtual void onInit() {
-    bool use_multithread;
-    ros::param::param<bool>("~use_multithread_callback", use_multithread, true);
-    if (use_multithread)
-    {
-      NODELET_DEBUG("Using multithread callback");
-      nh.reset (new ros::NodeHandle(getMTNodeHandle()));
-      pnh.reset (new ros::NodeHandle(getMTPrivateNodeHandle()));
-    }
-    else
-    {
-      NODELET_DEBUG("Using singlethread callback");
-      nh.reset(new ros::NodeHandle(getNodeHandle()));
-      pnh.reset(new ros::NodeHandle(getPrivateNodeHandle()));
-    }
+virtual void infoDisconnectionCallback(const ros::SingleSubscriberPublisher&) {
+  disconnectionCallbackImpl();
+}
 
-    // provider can be an url (e.g.: rtsp://10.0.0.1:554) or a number of device, (e.g.: 0 would be /dev/video0)
-    if (pnh->getParam("video_stream_provider", video_stream_provider)){
-        NODELET_INFO_STREAM("Resource video_stream_provider: " << video_stream_provider);
-        // If we are given a string of 4 chars or less (I don't think we'll have more than 100 video devices connected)
-        // treat is as a number and act accordingly so we open up the videoNUMBER device
-        if (video_stream_provider.size() < 4){
-            NODELET_INFO_STREAM("Getting video from provider: /dev/video" << video_stream_provider);
-            video_stream_provider_type = "videodevice";
-        }
-        else{
-            NODELET_INFO_STREAM("Getting video from provider: " << video_stream_provider);
-            if (video_stream_provider.find("http://") != std::string::npos ||
-                    video_stream_provider.find("https://") != std::string::npos){
-                video_stream_provider_type = "http_stream";
-            }
-            else if(video_stream_provider.find("rtsp://") != std::string::npos){
-                video_stream_provider_type = "rtsp_stream";
-            }
-            else {
-                // Check if file exists to know if it's a videofile
-                std::ifstream ifs;
-                ifs.open(video_stream_provider.c_str(), std::ifstream::in);
-                if (ifs.good()){
-                    video_stream_provider_type = "videofile";
-                }
-                else
-                    video_stream_provider_type = "unknown";
-            }
-        }
-    }
-    else{
-        NODELET_ERROR("Failed to get param 'video_stream_provider'");
-        return;
+virtual void configCallback(VideoStreamConfig& config, uint32_t level) {
+    NODELET_DEBUG("configCallback");
+    bool need_resubscribe = false;
+
+    if (camera_name != config.camera_name ||
+        camera_info_url != config.camera_info_url ||
+        frame_id != config.frame_id) {
+      camera_name = config.camera_name;
+      camera_info_url = config.camera_info_url;
+      frame_id = config.frame_id;
+      NODELET_INFO_STREAM("Camera name: " << camera_name);
+      NODELET_INFO_STREAM("Provided camera_info_url: '" << camera_info_url << "'");
+      NODELET_INFO_STREAM("Publishing with frame_id: " << frame_id);
+      camera_info_manager::CameraInfoManager cam_info_manager(*nh, camera_name, camera_info_url);
+      // Get the saved camera info if any
+      cam_info_msg = cam_info_manager.getCameraInfo();
+      cam_info_msg.header.frame_id = frame_id;
     }
 
-    NODELET_INFO_STREAM("Video stream provider type detected: " << video_stream_provider_type);
+    if (set_camera_fps != config.set_camera_fps ||
+        fps != config.fps) {
+      if (config.fps > config.set_camera_fps) {
+        NODELET_WARN_STREAM("Asked to publish at 'fps' (" << config.fps
+                            << ") which is higher than the 'set_camera_fps' (" << config.set_camera_fps <<
+                            "), we can't publish faster than the camera provides images.");
+        config.fps = config.set_camera_fps;
+      }
+      set_camera_fps = config.set_camera_fps;
+      fps = config.fps;
+      NODELET_INFO_STREAM("Setting camera FPS to: " << set_camera_fps);
+      NODELET_INFO_STREAM("Throttling to fps: " << fps);
+      need_resubscribe = true;
+    }
 
-    std::string camera_name;
-    pnh->param("camera_name", camera_name, std::string("camera"));
-    NODELET_INFO_STREAM("Camera name: " << camera_name);
+    if (max_queue_size != config.buffer_queue_size) {
+      max_queue_size = config.buffer_queue_size;
+      NODELET_INFO_STREAM("Setting buffer size for capturing frames to: " << max_queue_size);
+    }
 
-    pnh->param("set_camera_fps", set_camera_fps, 30.0);
-    NODELET_INFO_STREAM("Setting camera FPS to: " << set_camera_fps);
+    if (flip_horizontal != config.flip_horizontal ||
+        flip_vertical != config.flip_vertical) {
+      flip_horizontal = config.flip_horizontal;
+      flip_vertical = config.flip_vertical;
+      NODELET_INFO_STREAM("Flip horizontal image is: " << ((flip_horizontal)?"true":"false"));
+      NODELET_INFO_STREAM("Flip vertical image is: " << ((flip_vertical)?"true":"false"));
+    }
 
-    int buffer_queue_size;
-    pnh->param("buffer_queue_size", buffer_queue_size, 100);
-    NODELET_INFO_STREAM("Setting buffer size for capturing frames to: " << buffer_queue_size);
-    max_queue_size = buffer_queue_size;
-
-    pnh->param("fps", fps, 240.0);
-    NODELET_INFO_STREAM("Throttling to fps: " << fps);
-
-    if (video_stream_provider.size() < 4 && fps > set_camera_fps)
-        NODELET_WARN_STREAM("Asked to publish at 'fps' (" << fps
-                        << ") which is higher than the 'set_camera_fps' (" << set_camera_fps <<
-                        "), we can't publish faster than the camera provides images.");
-
-    pnh->param("frame_id", frame_id, std::string("camera"));
-    NODELET_INFO_STREAM("Publishing with frame_id: " << frame_id);
-
-    std::string camera_info_url;
-    pnh->param("camera_info_url", camera_info_url, std::string(""));
-    NODELET_INFO_STREAM("Provided camera_info_url: '" << camera_info_url << "'");
-
-    bool flip_horizontal;
-    pnh->param("flip_horizontal", flip_horizontal, false);
-    NODELET_INFO_STREAM("Flip horizontal image is: " << ((flip_horizontal)?"true":"false"));
-
-    bool flip_vertical;
-    pnh->param("flip_vertical", flip_vertical, false);
-    NODELET_INFO_STREAM("Flip vertical image is: " << ((flip_vertical)?"true":"false"));
-
-    pnh->param("width", width_target, 0);
-    pnh->param("height", height_target, 0);
-    if (width_target != 0 && height_target != 0){
+    if (width_target != config.width ||
+        height_target != config.height) {
+      width_target = config.width;
+      height_target = config.height;
+      if (width_target != 0 && height_target != 0) {
         NODELET_INFO_STREAM("Forced image width is: " << width_target);
         NODELET_INFO_STREAM("Forced image height is: " << height_target);
+      }
+      need_resubscribe = true;
     }
 
-    // From http://docs.opencv.org/modules/core/doc/operations_on_arrays.html#void flip(InputArray src, OutputArray dst, int flipCode)
-    // FLIP_HORIZONTAL == 1, FLIP_VERTICAL == 0 or FLIP_BOTH == -1
-    flip_image = true;
-    if (flip_horizontal && flip_vertical)
-        flip_value = -1; // flip both, horizontal and vertical
-    else if (flip_horizontal)
-        flip_value = 1;
-    else if (flip_vertical)
-        flip_value = 0;
-    else
-        flip_image = false;
+    loop_videofile = config.loop_videofile;
 
-    pnh->param("loop_videofile", loop_videofile, false);
+    if (subscriber_num > 0 && need_resubscribe) {
+      unsubscribe();
+      subscribe();
+    }
+}
 
-    std_msgs::Header header;
-    header.frame_id = frame_id;
-    camera_info_manager::CameraInfoManager cam_info_manager(*nh, camera_name, camera_info_url);
-    // Get the saved camera info if any
-    cam_info_msg = cam_info_manager.getCameraInfo();
-    cam_info_msg.header = header;
+virtual void onInit() {
+    nh.reset(new ros::NodeHandle(getNodeHandle()));
+    pnh.reset(new ros::NodeHandle(getPrivateNodeHandle()));
+    subscriber_num = 0;
 
+    // provider can be an url (e.g.: rtsp://10.0.0.1:554) or a number of device, (e.g.: 0 would be /dev/video0)
+    pnh->param<std::string>("video_stream_provider", video_stream_provider, "0");
+    // check file type
+    try {
+      int device_num = std::stoi(video_stream_provider);
+      video_stream_provider_type ="videodevice";
+    } catch (std::invalid_argument &ex) {
+      if (video_stream_provider.find("http://") != std::string::npos ||
+          video_stream_provider.find("https://") != std::string::npos){
+        video_stream_provider_type = "http_stream";
+      }
+      else if(video_stream_provider.find("rtsp://") != std::string::npos){
+        video_stream_provider_type = "rtsp_stream";
+      }
+      else{
+        fs::file_type file_type = fs::status(fs::path(video_stream_provider)).type();
+        switch (file_type) {
+        case fs::file_type::character_file:
+        case fs::file_type::block_file:
+          video_stream_provider_type = "videodevice";
+          break;
+        case fs::file_type::regular_file:
+          video_stream_provider_type = "videofile";
+          break;
+        default:
+          video_stream_provider_type = "unknown";
+        }
+      }
+    }
+
+    // set parameters from dynamic reconfigure server
+    dyn_srv = boost::make_shared<dynamic_reconfigure::Server<VideoStreamConfig> >(*pnh);
+    auto f = boost::bind(&VideoStreamNodelet::configCallback, this, _1, _2);
+    dyn_srv->setCallback(f);
+
+    subscriber_num = 0;
     image_transport::SubscriberStatusCallback connect_cb =
       boost::bind(&VideoStreamNodelet::connectionCallback, this, _1);
     ros::SubscriberStatusCallback info_connect_cb =
       boost::bind(&VideoStreamNodelet::infoConnectionCallback, this, _1);
+    image_transport::SubscriberStatusCallback disconnect_cb =
+      boost::bind(&VideoStreamNodelet::disconnectionCallback, this, _1);
+    ros::SubscriberStatusCallback info_disconnect_cb =
+      boost::bind(&VideoStreamNodelet::infoDisconnectionCallback, this, _1);
     pub = image_transport::ImageTransport(*nh).advertiseCamera(
       "image_raw", 1,
       connect_cb, connect_cb,
       info_connect_cb, info_connect_cb,
       ros::VoidPtr(), false);
+}
+
+virtual ~VideoStreamNodelet() {
+  if (subscriber_num > 0)
+    subscriber_num = 0;
+    unsubscribe();
 }
 };
 } // namespace
